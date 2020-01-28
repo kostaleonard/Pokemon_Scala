e511a1d626138604bcb35acb93f75b3dfe9c350c
leo <kostaleonard@gmail.com>
2020-01-28 18:26:10 -0500

    Prescaled images to make things run faster. Huge difference.

--- src/main/scala/model/board/cells/ConcretePavement.scala
+++ src/main/scala/model/board/cells/ConcretePavement.scala
@@ -5,21 +5,28 @@ import java.awt.image.BufferedImage
 import java.io.File
 
 import javax.imageio.ImageIO
+import model.board.Board
 import view.View
 
 object ConcretePavement {
   val CONCRETE_PAVEMENT_IMAGE: Image =
     ImageIO.read(new File(View.getSourcePath("tiles/concrete_path_1.png")))
+
+  /** Returns the prescaled image. */
+  protected def getPrescaledImage: BufferedImage = {
+    val bufferedImage = new BufferedImage(Board.TILE_SIZE, Board.TILE_SIZE, BufferedImage.TYPE_INT_ARGB)
+    val g2d = bufferedImage.getGraphics.asInstanceOf[Graphics2D]
+    g2d.drawImage(CONCRETE_PAVEMENT_IMAGE, 0, 0, Board.TILE_SIZE, Board.TILE_SIZE, null)
+    g2d.dispose()
+    bufferedImage
+  }
 }
 
 class ConcretePavement extends Cell {
+  val prescaledImage: BufferedImage = ConcretePavement.getPrescaledImage
+
   /** Returns the object's image, which should be drawn on the canvasImage. This image may be scaled later. */
-  override def getImage: BufferedImage = {
-    val g2d = canvasImage.getGraphics.asInstanceOf[Graphics2D]
-    g2d.drawImage(ConcretePavement.CONCRETE_PAVEMENT_IMAGE, 0, 0, getObjectWidth, getObjectHeight, null)
-    g2d.dispose()
-    canvasImage
-  }
+  override def getImage: BufferedImage = prescaledImage
 
   /** Progresses animations by one frame. Parent objects should call on all child objects they render. */
   override def advanceFrame(): Unit = {}
--- src/main/scala/model/board/cells/Dirt.scala
+++ src/main/scala/model/board/cells/Dirt.scala
@@ -5,20 +5,27 @@ import java.awt.image.BufferedImage
 import java.io.File
 
 import javax.imageio.ImageIO
+import model.board.Board
 import view.View
 
 object Dirt {
   val DIRT_IMAGE: Image = ImageIO.read(new File(View.getSourcePath("tiles/dirt_path_1.png")))
+
+  /** Returns the prescaled image. */
+  protected def getPrescaledImage: BufferedImage = {
+    val bufferedImage = new BufferedImage(Board.TILE_SIZE, Board.TILE_SIZE, BufferedImage.TYPE_INT_ARGB)
+    val g2d = bufferedImage.getGraphics.asInstanceOf[Graphics2D]
+    g2d.drawImage(DIRT_IMAGE, 0, 0, Board.TILE_SIZE, Board.TILE_SIZE, null)
+    g2d.dispose()
+    bufferedImage
+  }
 }
 
 class Dirt extends Cell {
+  val prescaledImage: BufferedImage = Dirt.getPrescaledImage
+
   /** Returns the object's image, which should be drawn on the canvasImage. This image may be scaled later. */
-  override def getImage: BufferedImage = {
-    val g2d = canvasImage.getGraphics.asInstanceOf[Graphics2D]
-    g2d.drawImage(Dirt.DIRT_IMAGE, 0, 0, getObjectWidth, getObjectHeight, null)
-    g2d.dispose()
-    canvasImage
-  }
+  override def getImage: BufferedImage = prescaledImage
 
   /** Progresses animations by one frame. Parent objects should call on all child objects they render. */
   override def advanceFrame(): Unit = {}
